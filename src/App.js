86bef25627a56113d43e79dc265dfc9d8088b150
amitkinor <amit.kinor@gmail.com>
2020-07-30 15:32:24 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import Blog from './containers/Blog/Blog';
+import classes from './App.module.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className={classes.App}>
+        <Blog />
+      </div>
+    );
+  }
+}
+
+export default App;
